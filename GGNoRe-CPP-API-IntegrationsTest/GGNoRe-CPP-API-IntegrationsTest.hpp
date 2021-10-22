b1d4b2aa455a9f9a1c94c5650f8099760129d95e
Erebnyx <erebnyxstudio@gmail.com>
2021-10-22 12:03:32 +0900

    Savestates and pimpl setup
    - Savestates API
    - pimpl ABI
    - mute template export warning

--- GGNoRe-CPP-API-IntegrationsTest/GGNoRe-CPP-API-IntegrationsTest.hpp
+++ GGNoRe-CPP-API-IntegrationsTest/GGNoRe-CPP-API-IntegrationsTest.hpp
@@ -5,9 +5,23 @@
 #pragma once
 
 #include <Core/Data/DATA_CFG.hpp>
+#include <Rollback/ABS_CPT_RB_SaveStates.hpp>
+
+class TEST_CPT_RB_SaveStates : public GGNoRe::API::ABS_CPT_RB_SaveStates
+{
+protected:
+	void OnSerialize(std::vector<uint8_t>& TargetBuffer) {}
+	void OnDeserialize(const std::vector<uint8_t>& SourceBuffer) {}
+};
 
 int main()
 {
 	GGNoRe::API::DATA_CFG::Get().LogHumanReadable("LINKER TEST");
+
+	TEST_CPT_RB_SaveStates SaveStates;
+	SaveStates.Serialize(1);
+	SaveStates.Serialize(2);
+	SaveStates.Deserialize(1);
+
 	return 0;
 }
