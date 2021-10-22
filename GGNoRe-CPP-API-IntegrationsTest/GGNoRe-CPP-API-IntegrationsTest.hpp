0e701ce282a278016f437d595a31167943bf82ed
Erebnyx <erebnyxstudio@gmail.com>
2021-10-22 17:13:40 +0900

    Simulator
    - component that manages the simulation and lifetime of actors that can be rolled back

--- GGNoRe-CPP-API-IntegrationsTest/GGNoRe-CPP-API-IntegrationsTest.hpp
+++ GGNoRe-CPP-API-IntegrationsTest/GGNoRe-CPP-API-IntegrationsTest.hpp
@@ -6,12 +6,26 @@
 
 #include <Core/Data/DATA_CFG.hpp>
 #include <Rollback/ABS_CPT_RB_SaveStates.hpp>
+#include <Rollback/ABS_CPT_RB_Simulator.hpp>
 
 class TEST_CPT_RB_SaveStates : public GGNoRe::API::ABS_CPT_RB_SaveStates
 {
 protected:
-	void OnSerialize(std::vector<uint8_t>& TargetBuffer) {}
-	void OnDeserialize(const std::vector<uint8_t>& SourceBuffer) {}
+	void OnSerialize(std::vector<uint8_t>& TargetBuffer) override {}
+	void OnDeserialize(const std::vector<uint8_t>& SourceBuffer) override {}
+};
+
+class TEST_CPT_RB_Simulator : public GGNoRe::API::ABS_CPT_RB_Simulator
+{
+public:
+	TEST_CPT_RB_Simulator(const uint32_t SpawnFrameIndex)
+		:ABS_CPT_RB_Simulator(SpawnFrameIndex)
+	{
+	}
+
+protected:
+	virtual void OnSimulateFrame(const float FrameDurationInSeconds) override {}
+	virtual void OnEndOfLife() override {}
 };
 
 int main()
@@ -19,9 +33,14 @@ int main()
 	GGNoRe::API::DATA_CFG::Get().LogHumanReadable("LINKER TEST");
 
 	TEST_CPT_RB_SaveStates SaveStates;
+	TEST_CPT_RB_Simulator Simulator(0);
+
+	Simulator.SimulateFrame(0);
+	SaveStates.Serialize(0);
+	Simulator.SimulateFrame(1);
 	SaveStates.Serialize(1);
-	SaveStates.Serialize(2);
-	SaveStates.Deserialize(1);
+	SaveStates.Deserialize(0);
+	Simulator.SimulateFrame(0);
 
 	return 0;
 }
