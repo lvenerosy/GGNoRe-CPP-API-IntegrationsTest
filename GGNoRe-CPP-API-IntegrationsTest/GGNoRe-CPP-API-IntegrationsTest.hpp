ddcb57a43548ad0f0c48b45abe81e6e382aa6e2c
Erebnyx <erebnyxstudio@gmail.com>
2022-03-13 13:00:01 +0100

    Fix saving frame to only hold the buffer size

--- GGNoRe-CPP-API-IntegrationsTest/GGNoRe-CPP-API-IntegrationsTest.hpp
+++ GGNoRe-CPP-API-IntegrationsTest/GGNoRe-CPP-API-IntegrationsTest.hpp
@@ -10,10 +10,10 @@ bool Test1Local2RemoteMockRollback(const bool UseFakeRollback, const bool UseRan
 
 int main()
 {
-	//assert(Test1Local2RemoteMockRollback(false, false));
+	assert(Test1Local2RemoteMockRollback(false, false));
 	assert(Test1Local2RemoteMockRollback(true, false));
-	//assert(Test1Local2RemoteMockRollback(false, true));
-	//assert(Test1Local2RemoteMockRollback(true, true));
+	assert(Test1Local2RemoteMockRollback(false, true));
+	assert(Test1Local2RemoteMockRollback(true, true));
 
 	return 0;
 }
